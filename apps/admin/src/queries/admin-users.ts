943c178eb1d71b64e65e253c919e95c35a277447
Hammad Faheem <hammadfaheem5@gmail.com>
2026-04-20 04:29:06 +0500

    Revert "feat(workspace): add admin app and update dependencies"
    This reverts commit 90c93ab6264e555bf2841698ed07d12ca7ee5ef2.

--- apps/admin/src/queries/admin-users.ts
+++
@@ -1,9 +0,0 @@
-import { queryOptions } from '@tanstack/react-query'
-import { api } from '@/utils/api'
-import type { AdminUser } from '@/types/api'
-
-export const adminUsersQueryOptions = () =>
-  queryOptions({
-    queryKey: ['admin', 'users'],
-    queryFn: () => api.get<AdminUser[]>('/api/admin/users').then((r) => r.data),
-  })
